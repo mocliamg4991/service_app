28b30328ae2198679953a1d38af3f8bf826971a9
mocliamg4991 <60917945+mocliamg4991@users.noreply.github.com>
2023-01-04 14:25:13 +0600

    create serializer and optimize queries

--- service/service/urls.py
+++ service/service/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from services.views import SubscriptionView
+
+router = DefaultRouter()
+router.register("subscriptions", SubscriptionView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
+ 
+ 
--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import Subscription
+from clients.models import Client
+from .serializers import SubscriptionSerializer
+from django.db.models import Prefetch
 
-# Create your views here.
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch(
+            'client',queryset = Client.objects.select_related('user').only(
+                'company_name',
+                'user__email'
+            )
+        )
+    )
+    serializer_class = SubscriptionSerializer
+

5203e55c948d7c9de7901a386f5bb43525246507
mocliamg4991 <60917945+mocliamg4991@users.noreply.github.com>
2023-01-04 10:53:20 +0600

    creating models

--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
+from clients.models import Client
 # Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full','Full'),
+        ('student','Student'),
+        ('discount','Discount'),
+    )
+
+    plan_type = models.CharField(choices = PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0,
+                                                    validators=[
+                                                        MaxValueValidator()
+                                                    ])
+
+class Subscription(models.Model):
+    client =  models.ForeignKey(Client, on_delete = models.PROTECT, related_name = 'subscriptions')
+    service =  models.ForeignKey(Service, on_delete = models.PROTECT, related_name = 'subscriptions')
+    plan =  models.ForeignKey(Plan, on_delete = models.PROTECT, related_name = 'subscriptions')

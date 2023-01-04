35f2c51ce5fa4e7e73ae489f9d6f463a429c6dbc
mocliamg4991 <60917945+mocliamg4991@users.noreply.github.com>
2023-01-04 11:30:26 +0600

    add test data

--- service/clients/models.py
+++ service/clients/models.py
@@ -6,3 +6,5 @@ class Client(models.Model):
     company_name = models.CharField(max_length=100)
     full_address = models.CharField(max_length=100)
     
+    def __str__(self):
+        return f"Client: {self.company_name}"

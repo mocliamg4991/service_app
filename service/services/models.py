c5f891e50e0cb11a28236434ae15588f7db10178
mocliamg4991 <60917945+mocliamg4991@users.noreply.github.com>
2023-01-26 01:12:32 +0600

    add comment field, add celery task

--- service/services/models.py
+++ service/services/models.py
@@ -2,20 +2,25 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from clients.models import Client
 # Create your models here.
-from .tasks import set_price
+from .tasks import set_price, set_comment
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return 'service  - {0}'.format(self.name)
 
     def __init__(self, *args, **kwargs):    
         super().__init__(*args, **kwargs)
         self.__full_price = self.full_price   
 
     def save(self, *args,  **kwargs):
+        super().save(*args, **kwargs)
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
 class Plan(models.Model):
@@ -31,14 +36,19 @@ class Plan(models.Model):
                                                         MaxValueValidator(100)
                                                     ])
     
+    def __str__(self) -> str:
+        return 'plan - {0}'.format(self.plan_type)
+
     def __init__(self, *args, **kwargs):    
         super().__init__(*args, **kwargs)
         self.__discount_percent = self.discount_percent   
 
     def save(self, *args,  **kwargs):
+        super().save(*args, **kwargs)
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
 class Subscription(models.Model):
@@ -46,5 +56,7 @@ class Subscription(models.Model):
     service =  models.ForeignKey(Service, on_delete = models.PROTECT, related_name = 'subscriptions')
     plan =  models.ForeignKey(Plan, on_delete = models.PROTECT, related_name = 'subscriptions')
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
-   
+    def __str__(self) -> str:
+        return '{0}, {1}, {2}'.format(self.client, self.service, self.plan)
--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,11 +1,34 @@
+import time
 from celery import shared_task
 from django.db.models import F
+import datetime
+from django.db import transaction
 
 @shared_task
 def set_price(subscription_id):
-    from services.models import Subscription
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(annotated_price=
-            F('service__full_price') -
-            F('service__full_price') * (F('plan__discount_percent')/100.00)).first()
-    subscription.price = subscription.annotated_price
-    subscription.save()
+
+    from services.models import Subscription    
+    with transaction.atomic():
+
+
+
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(annotated_price=F('service__full_price') -
+                                                                                F('service__full_price') * (F('plan__discount_percent')/100.00)).first()
+
+
+        subscription.price = subscription.annotated_price
+        subscription.save()
+    print('do_something')
+
+@shared_task
+def set_comment(subscription_id):
+
+    from services.models import Subscription  
+    print('do_something')
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
+    print('do_something')
